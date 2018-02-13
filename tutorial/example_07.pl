e3c254178f3dc68a5be0b3f5a92331a786c976bf
Steven Bakker <steven.bakker@ams-ix.net>
2018-02-13 14:19:59 +0100

    Add tutorial text & code.

---
+++ tutorial/example_07.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+use FindBin;
+use lib ("$FindBin::Bin/../lib");
+
+use Term::CLI;
+
+$SIG{INT} = 'IGNORE';
+
+my @commands;
+my $term = Term::CLI->new(
+	name     => 'bssh',             # A basically simple shell.
+	skip     => qr/^\s*(?:#.*)?$/,  # Skip comments and empty lines.
+	prompt   => 'bssh> ',           # A more descriptive prompt.
+	commands => \@commands,
+);
+
+push @commands, Term::CLI::Command->new(
+	name => 'exit',
+	callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        execute_exit($cmd->name, @{$args{arguments}});
+        return %args;
+    },
+	arguments => [
+		Term::CLI::Argument::Number::Int->new(  # Integer
+            name => 'excode',
+			min => 0,             # non-negative
+			inclusive => 1,       # "0" is allowed
+			min_occur => 0,       # occurrence is optional
+			max_occur => 1,       # no more than once
+		),
+	],
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'echo',
+    arguments => [
+        Term::CLI::Argument::String->new( name => 'arg',
+            min_occur => 0, max_occur => 0
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        say "@{$args{arguments}}";
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'make',
+    arguments => [
+        Term::CLI::Argument::Enum->new( name => 'target',
+            value_list => [qw( love money)],
+        ),
+        Term::CLI::Argument::Enum->new( name => 'when',
+            value_list => [qw( now later never forever )],
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        my @args = @{$args{arguments}};
+        say "making $args[0] $args[1]";
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'ls',
+    arguments => [
+        Term::CLI::Argument::Filename->new( name => 'arg',
+            min_occur => 0, max_occur => 0
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        my @args = @{$args{arguments}};
+        system('ls', @args);
+        $args{status} = $?;
+        return %args;
+    }
+);
+
+sub execute_exit {
+    my ($cmd, $excode) = @_;
+    $excode //= 0;
+    say "-- $cmd: $excode";
+    exit $excode;
+}
+
+say "\n[Welcome to BSSH]";
+while ( defined(my $line = $term->readline) ) {
+    $term->execute($line);
+}
+print "\n";
+execute_exit('exit', 0);
---
+++ tutorial/example_10.pl
@@ -0,0 +1,177 @@
+#!/usr/bin/perl
+
+use Modern::Perl;
+use FindBin;
+use lib ("$FindBin::Bin/../lib");
+
+use Term::CLI;
+
+$SIG{INT} = $SIG{QUIT} = 'IGNORE';
+
+my @commands;
+my $term = Term::CLI->new(
+	name     => 'bssh',             # A basically simple shell.
+	skip     => qr/^\s*(?:#.*)?$/,  # Skip comments and empty lines.
+	prompt   => 'bssh> ',           # A more descriptive prompt.
+	commands => \@commands,
+);
+
+push @commands, Term::CLI::Command->new(
+	name => 'exit',
+	callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        execute_exit($cmd->name, @{$args{arguments}});
+        return %args;
+    },
+	arguments => [
+		Term::CLI::Argument::Number::Int->new(  # Integer
+            name => 'excode',
+			min => 0,             # non-negative
+			inclusive => 1,       # "0" is allowed
+			min_occur => 0,       # occurrence is optional
+			max_occur => 1,       # no more than once
+		),
+	],
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'echo',
+    arguments => [
+        Term::CLI::Argument::String->new( name => 'arg',
+            min_occur => 0, max_occur => 0
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        say "@{$args{arguments}}";
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'make',
+    arguments => [
+        Term::CLI::Argument::Enum->new( name => 'target',
+            value_list => [qw( love money)],
+        ),
+        Term::CLI::Argument::Enum->new( name => 'when',
+            value_list => [qw( now later never forever )],
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        my @args = @{$args{arguments}};
+        say "making $args[0] $args[1]";
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'ls',
+    arguments => [
+        Term::CLI::Argument::Filename->new( name => 'arg',
+            min_occur => 0, max_occur => 0
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        my @args = @{$args{arguments}};
+        do {
+            system('ls', @args);
+        };
+        $args{status} = $?;
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'cp',
+    arguments => [
+        Term::CLI::Argument::Filename->new( name => 'path',
+            min_occur => 2,
+            max_occur => 0
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+        my @src = @{$args{arguments}};
+		my $dst = pop @src;
+		
+		say "command:     ".$cmd->name;
+		say "source:      ".join(', ', @src);
+		say "destination: ".$dst;
+
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'sleep',
+    arguments => [
+        Term::CLI::Argument::Number::Int->new( name => 'time',
+            min => 1, inclusive => 1
+        ),
+    ],
+    callback => sub {
+        my ($cmd, %args) = @_;
+        return %args if $args{status} < 0;
+
+        my $time = $args{arguments}->[0];
+
+        say "-- sleep: $time";
+
+        my %oldsig = %::SIG; # Save signals;
+
+        # Make sure we can interrupt the sleep() call.
+        $::SIG{INT} = $::SIG{QUIT} = sub {
+            say STDERR "(interrupted by $_[0])";
+        };
+
+        my $slept = sleep($time);
+
+        %::SIG = %oldsig; # Restore signal handlers.
+
+        say "-- woke up after $slept sec", $slept == 1 ? '' : 's';
+        return %args;
+    }
+);
+
+push @commands, Term::CLI::Command->new(
+    name => 'show',
+    commands => [
+        Term::CLI::Command->new( name => 'clock',
+            callback => sub {
+                my ($self, %args) = @_;
+                say scalar(localtime);
+                return %args;
+            },
+        ),
+        Term::CLI::Command->new( name => 'load',
+            callback => sub {
+                my ($self, %args) = @_;
+                system('uptime');
+                $args{status} = $?;
+                return %args;
+            },
+        ),
+    ],
+);
+
+sub execute_exit {
+    my ($cmd, $excode) = @_;
+    $excode //= 0;
+    say "-- $cmd: $excode";
+    exit $excode;
+}
+
+say "\n[Welcome to BSSH]";
+while ( defined(my $line = $term->readline) ) {
+    $term->execute($line);
+}
+print "\n";
+execute_exit('exit', 0);
