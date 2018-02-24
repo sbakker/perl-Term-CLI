10afeae9d4678dbdd38f5f037555c851e41a6368
Steven Bakker <steven.bakker@ams-ix.net>
2018-02-24 16:06:12 +0545

    Add some new files.

---
+++ scratch/pod_test.pl
@@ -0,0 +1,53 @@
+use Modern::Perl;
+
+use Pod::Simple;
+use Pod::Text::Termcap;
+
+my $formatted;
+my $parser = Pod::Text::Termcap->new( width => 39 );
+$parser->output_string(\$formatted);
+
+my $usage_1 = "B<file> {B<cp>|B<info>|B<mv>}";
+my $summary = "Perform various file operations";
+
+$parser->parse_string_document(<<EOF);
+=over 40
+
+=item $usage_1
+
+$summary
+
+=back
+EOF
+
+$formatted =~ s/\n+$//s;
+$formatted =~ s/^\s+//s;
+print "$formatted\n";
+
+$formatted = '';
+$parser = Pod::Text::Termcap->new( width => 39 );
+$parser->output_string(\$formatted);
+
+my @usage_2 = (
+    "B<file> [B<--verbose>] {B<cp>|B<info>|B<mv>}",
+    "B<file> [B<-v>] {B<cp>|B<info>|B<mv>}",
+);
+
+my $description =<<EOF;
+Perform various file operations, depending on the sub-command
+given.
+EOF
+
+my $text = "=head2 Usage:\n\n=over\n\n"
+        . join("", map { "=item $_\n\n" } @usage_2)
+        . "=back\n\n"
+        . "=head2 Description:\n\n"
+        . $description;
+
+$parser->parse_string_document($text);
+#$formatted =~ s/\n+$//s;
+#$formatted =~ s/^\s+//s;
+print "---\n";
+print "$formatted\n";
+print "---\n";
+print "$text";
---
+++ scratch/rename_tests.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use Modern::Perl;
+
+my @files = qw(
+00-compile.t
+01-pod-coverage.t
+02-pod.t
+05-Term-CLI-Element.t
+10-Term-CLI-Argument.t
+11-Term-CLI-Argument-Enum.t
+12-Term-CLI-Argument-Filename.t
+13-Term-CLI-Argument-Number.t
+14-Term-CLI-Argument-Number-Int.t
+15-Term-CLI-Argument-Number-Float.t
+16-Term-CLI-Command.t
+17-Term-CLI.t
+18-Term-CLI-ReadLine.t
+);
+
+my $n = 0;
+for my $f (@files) {
+    my $f2 = $f;
+    $f2 =~ s/^\d+-/sprintf("%03d-", $n)/e;
+    $n += 5;
+    say "git mv $f $f2";
+}
