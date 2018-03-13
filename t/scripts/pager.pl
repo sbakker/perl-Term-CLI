268374265de3d4457f602860abf3fbd39961f34a
Steven Bakker <steven.bakker@ams-ix.net>
2018-03-13 21:48:59 +0100

    Fake pager should drain STDIN before exiting.

--- t/scripts/pager.pl
+++ t/scripts/pager.pl
@@ -1,1 +1,14 @@
-exit 1;
+#!perl
+#
+# Fake "pager" script.
+#
+# Usage: pager.pl <exitcode>
+#
+# Script drains STDIN and exits with <exitcode>.
+#
+
+my $status = shift @ARGV;
+
+while (<>) { }
+
+exit $status // 0;
