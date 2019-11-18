477270aa36edf8a2af232477600716b29c872a4c
Steven Bakker <steven.bakker@ams-ix.net>
2019-11-18 21:38:14 +0100

    Autoincrement version if argument is +1.

--- scratch/fix_version_number.pl
+++ scratch/fix_version_number.pl
@@ -13,15 +13,47 @@ if (!@ARGV || @ARGV < 2 || $ARGV[0] =~ /^-/) {
 
 our $VERSION = shift @ARGV;
 
+my $auto_increment = 0;
+
+if ($VERSION eq '+1') {
+    $auto_increment = 1;
+}
+elsif ($VERSION !~ /^\d+(?:\.\d+)?/) {
+    die "$0: bad version format '$VERSION'\n";
+}
+
 local($^I) = '.bak';
 local($/)  = undef;
 
 while (<>) {
     # Fix version number.
-    s/^(\s* package \s+ \S+) (?:\s* \S+)? \s* \{/$1  $VERSION {/xm;
+    s(  ^
+        (?<head>   \s* package \s+ \S+)
+        (?<version> (?:\s* \S+)?)
+        (?<tail>    \s* \{)
+    )(
+        $+{head} . "  " . new_version($+{version}) . $+{tail}
+    )exm;
 
     # Update classic $VERSION assignment.
-    s/^(\s* our \s+ \$VERSION \s* = \s* )\S+( \s* ;\s*)/$1$VERSION$2/xm;
+    s(  ^
+        (?<head>    \s* our \s+ \$VERSION \s* = \s* )
+        (?<version> \S+)
+        (?<tail>    \s* ;\s*)
+    )(
+        $+{head} . new_version($+{version}) . $+{tail}
+    )exm;
 
     print;
+}
+
+sub new_version {
+    my ($cur) = @_;
+
+    return $VERSION if !$auto_increment;
+    return '0.01' if !$cur;
+    my $add = $cur =~ s/0+$//gr;
+    $add =~ s/\d/0/g;
+    $add =~ s/\d$/1/;
+    return $cur + $add;
 }
