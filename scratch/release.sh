36923a6716fdf104ac13d88976681d64d5db2e3f
Steven Bakker <steven.bakker@ams-ix.net>
2021-05-19 12:09:00 +0200

    Make versioning a bit more robust.

--- scratch/release.sh
+++ scratch/release.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-VERSION=v$(perl scratch/get_version.pl Term::CLI)
+PROG=$0
+DIR=$(dirname $PROG)
+
+VERSION=v$(perl $DIR/get_version.pl Term::CLI)
+
+[[ -n $VERSION ]] || exit 1
+
+echo Releasing $VERSION
+
 git commit -a -m "Release $VERSION"
 git tag -m "Release $VERSION" $VERSION
 git push
